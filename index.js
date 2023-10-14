544b871b3c93ecc82dd038e48edcde9457fbbad5
souf139 <soufianboushaba12@gmail.com>
2023-10-14 18:10:57 +0100

    this commit for the second page  : page detail product responsive

--- index.js
+++ index.js
@@ -10,3 +10,37 @@
         }
     });
 
+
+
+const Input = document.getElementById("quantity");
+const incButton = document.getElementById("increment");
+const dicButton = document.getElementById("dicrement");
+
+incButton.addEventListener("click", function() {
+    Input.value = parseInt(Input.value) + 1;
+});
+
+dicButton.addEventListener("click", function() {
+    if (parseInt(Input.value) > 1) {
+        Input.value = parseInt(Input.value) - 1;
+    }
+});
+
+
+
+
+
+
+
+const line = document.querySelectorAll(".product-link");
+const contenu = document.querySelectorAll(".product-content");
+
+line.forEach(function (link) {
+    link.addEventListener("click", function () {
+        const id = link.getAttribute("data-target");
+        contenu.forEach(function (content) {
+            content.style.display = "none";
+        });
+        document.getElementById(id).style.display = "flex";
+    });
+});
